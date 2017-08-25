cd9ae1c0008dccd044064b531dcb6d7a4defcbbe
Geravant <igor.gera.gerasim@gmail.com>
2017-08-25 18:24:01 +0300

    Why not?

--- src/MyFileVisitor.java
+++ src/MyFileVisitor.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class MyFileVisitor extends SimpleFileVisitor<Path> {
     private JTree directoryTree;
     private DefaultMutableTreeNode top;
+    private TreePath[] treePathes;
+    private int count = 0;
 
     MyFileVisitor(String dirname) {
         this.top = new DefaultMutableTreeNode(dirname);
@@ -19,12 +23,18 @@ public class MyFileVisitor extends SimpleFileVisitor<Path> {
         System.out.println(path);
         DefaultMutableTreeNode node = new DefaultMutableTreeNode(path.getFileName());
         top.add(node);
-        System.out.println(node.getParent()+node.toString());
+        System.out.println(node.getParent()+"\\"+node.toString());
+        this.treePathes[count] = new TreePath(node);
+        count++;
         return FileVisitResult.CONTINUE;
     }
 
     public JTree getTree() {
         this.directoryTree  = new JTree(top);
         return this.directoryTree;
+    }
+
+    public TreePath[] getPathes() {
+        return treePathes;
     }
 }
--- src/VisitorOfFiles.java
+++ src/VisitorOfFiles.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeExpansionListener;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.io.*;
@@ -38,15 +41,32 @@ class VisitorOfFiles {
         final JLabel jLab = new JLabel();
         jfrm.add(jLab, BorderLayout.SOUTH);
 
-        tree.addTreeSelectionListener(new TreeSelectionListener() {
+        tree.addTreeSelectionListener(new TreeExpansionListener() {
             @Override
-            public void valueChanged(TreeSelectionEvent e) {
-                jLab.setText("Selection is "+ e.getPath());
-                MyFileVisitor myFileVisitor = new MyFileVisitor("C:\\Users");
+            public void valueChanged(TreeExpansionEvent e) {
+                int dirDepth = e.path().getPathCount();
+                String dirname = e.getPath().getPathComponent(0).toString();
+                for(int i=1; i< dirDepth; i++) {
+                    dirname = dirname + "\\"+ e.getPath().getPathComponent(i);
+                    System.out.println(dirname);
+                }
+                //dirname.replaceAll(", ", "\\");
+                //jLab.setText("Selection is "+ dirname);
+
+                MyFileVisitor myFileVisitor = new MyFileVisitor(dirname);
                 try {
-                    Files.walkFileTree(Paths.get("C:\\Users"), EnumSet.of(FileVisitOption.FOLLOW_LINKS), 1, myFileVisitor);
+                    Files.walkFileTree(Paths.get(dirname), EnumSet.of(FileVisitOption.FOLLOW_LINKS), 1, myFileVisitor);
                 } catch (IOException exc) {
                     System.out.println("IO Error");
+                }
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode)(e.getPath().getLastPathComponent());
+
+                tree.Getmodel();
+
+
+                for (TreePath treepath: myFileVisitor.getPathes()) {
+                    MutableTreeNode newChild = (MutableTreeNode) treepath;
+                    node.add(newChild);
                 }
 
             }
