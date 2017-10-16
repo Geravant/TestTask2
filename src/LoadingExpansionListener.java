ce920b01ad78bbcf4e67033d8e9da54fe7f89092
Geravant <igor.gera.gerasim@gmail.com>
2017-10-16 13:36:52 +0300

    Now with multithreading

--- src/LoadingExpansionListener.java
+++ src/LoadingExpansionListener.java
@@ -4,6 +4,8 @@ import javax.swing.event.TreeExpansionListener;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeSelectionModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoadingExpansionListener implements TreeExpansionListener{
     private GUI programGUI;
@@ -17,42 +19,31 @@ public class LoadingExpansionListener implements TreeExpansionListener{
 
         final Thread currentThread = Thread.currentThread();
 
+
         final LazyLoad lazyload = new LazyLoad(e);
+        final TreeExpansionEvent event = e;
 
         programGUI.getLoadCellRenderer().setCurrentLoading(lazyload.getCurrentLoading().toString());
         programGUI.getLoadCellRenderer().setCurrentLoadingNode(lazyload.getNode());
-        programGUI.getLoadCellRenderer().setCurrentLoadingNodePath(e.getPath());
+//        programGUI.getLoadCellRenderer().setCurrentLoadingNodePath(e.getPath());
         FileTreeNode plugNode =(FileTreeNode) lazyload.getNode().getChildAt(0);
         programGUI.getLoadCellRenderer().setPlug(plugNode.getUserObject().toString());
         programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
-        Thread lazyloadrun = new Thread(lazyload);
-        SwingUtilities.invokeLater(lazyload);
-        SwingUtilities.invokeLater(new Runnable() {
+//        final Thread lazyloadrun = new Thread(lazyload);
+
+//        SwingUtilities.invokeLater(lazyload);
+//        lazyloadrun.run();
+        ActionListener listener = new ActionListener() {
             @Override
-            public void run() {
-                programGUI.getLoadCellRenderer().setCurrentLoading(null);
-                programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+            public void actionPerformed(ActionEvent a) {
+
+                programGUI.getLoadCellRenderer().setCurrentLoadingNodePath(event.getPath());
+                programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
+                LazyLoad localLazyLoad = lazyload;
+                Thread lazyloadrun = new Thread(localLazyLoad);
+                System.out.println("Lazyload started for" + localLazyLoad.getCurrentLoading().toString());
+                lazyloadrun.run();
                 programGUI.getTreeModel().nodeStructureChanged(programGUI.getLoadCellRenderer().getCurrentLoadingNode());
-                try {
-                    Thread.sleep(2000);
-
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
-                }
-                currentThread.run();
-                programGUI.getTreeModel().nodeStructureChanged(programGUI.getLoadCellRenderer().getCurrentLoadingNode());
-
-            }
-        });
-        lazyload.getNode().setLoading(true);
-
-        programGUI.getLoadCellRenderer().setCurrentLoading(lazyload.getCurrentLoading().toString());
-
-        programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
                 TreeModel sub = new DefaultTreeModel(lazyload.getNode());
                 programGUI.setFolder(new JTree(sub));
                 programGUI.getFolder().addMouseListener(new ActionsMenu(programGUI.getJpu()));
@@ -73,17 +64,87 @@ public class LoadingExpansionListener implements TreeExpansionListener{
                 else componentToRemove = programGUI.getContent().getComponentZOrder(jspSubFolder)-1;
                 programGUI.getContent().remove(componentToRemove);
                 programGUI.getContent().validate();
-            }
-        });
+                programGUI.getLoadCellRenderer().setCurrentLoading(null);
+                programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+                programGUI.getTreeModel().nodeStructureChanged(programGUI.getLoadCellRenderer().getCurrentLoadingNode());
+//                try {
+//                    Thread.sleep(2000);
+//
+//                } catch (InterruptedException e1) {
+//                    e1.printStackTrace();
+//                }
+                programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+                programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
+                currentThread.run();
 
-        programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+            }
+        };
+        Timer timer = new Timer(2000, listener);
+        timer.start();
+
+
+//            @Override
+//            public void run() {
+//                System.out.println("Entering lazyload");
+//                programGUI.getLoadCellRenderer().setCurrentLoading(null);
+//                programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+//                programGUI.getTreeModel().nodeStructureChanged(programGUI.getLoadCellRenderer().getCurrentLoadingNode());
+//                try {
+//                    Thread.sleep(2000);
+//
+//                } catch (InterruptedException e1) {
+//                    e1.printStackTrace();
+//                }
+//                currentThread.run();
+//                programGUI.getTreeModel().nodeStructureChanged(programGUI.getLoadCellRenderer().getCurrentLoadingNode());
+//
+//            }
+//        });
+        lazyload.getNode().setLoading(true);
+
+        programGUI.getLoadCellRenderer().setCurrentLoading(lazyload.getCurrentLoading().toString());
+
         programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
+
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                TreeModel sub = new DefaultTreeModel(lazyload.getNode());
+//                programGUI.setFolder(new JTree(sub));
+//                programGUI.getFolder().addMouseListener(new ActionsMenu(programGUI.getJpu()));
+//                programGUI.getJpu().setCurrentFolder(lazyload.getDirname());
+//                programGUI.getJmb().setCurrentFolder(lazyload.getDirname());
+//                programGUI.getFolder().setRootVisible(false);
+//                programGUI.getFolder().setEditable(true);
+//                programGUI.getFolder().getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+//                programGUI.getFolder().setShowsRootHandles(false);
+//                programGUI.getFolder().setRootVisible(false);
+//                programGUI.getFolder().setCellRenderer(programGUI.getLoadCellRenderer());
+//                JScrollPane jspSubFolder = new JScrollPane(programGUI.getFolder());
+//                programGUI.getContent().add(jspSubFolder);
+//                int componentToRemove;
+//                if (programGUI.getContent().getComponentZOrder(programGUI.getJspFolder()) > 0) {
+//                    componentToRemove = programGUI.getContent().getComponentZOrder(programGUI.getJspFolder());
+//                }
+//                else componentToRemove = programGUI.getContent().getComponentZOrder(jspSubFolder)-1;
+//                programGUI.getContent().remove(componentToRemove);
+//                programGUI.getContent().validate();
+//            }
+//        });
+
+//        programGUI.getLoadCellRenderer().removeCurrentLoading(lazyload.getCurrentLoading().toString());
+//        programGUI.getTreeModel().nodeStructureChanged(lazyload.getNode());
 
     }
     @Override
     public void treeCollapsed(TreeExpansionEvent e) {
         LazyLoad lazyload = new LazyLoad(e);
+        lazyload.getNode().removeAllChildren();
         lazyload.getNode().setDir(true);
+        FileTreeNode plug = new FileTreeNode("Empty folder");
+
+        lazyload.getNode().add(plug);
+
     }
 
     public void setProgramGUI(GUI programGUI) {
